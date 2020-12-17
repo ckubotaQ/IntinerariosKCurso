b5bfe16295cf1a603b6341b5dcfdeae571d159ed
ckubotaQ <ckubota@quaxar.com>
2020-12-17 16:50:59 -0600

    update de app.json

--- src/const/index.js
+++ src/const/index.js
@@ -8,3 +8,8 @@ export const LOGIN= 'Login';
 export const HOME= 'Home'; 
 export const RESULTS= 'Results';
 export const PROFILE= 'Profile';
+/**
+ * Constantes para fuentes
+ */
+export const ROBOTO_FONT= 'Roboto';
+export const ROBOTO_MEDIUM_FONT = 'Roboto_medium';
--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 
-import React from 'react'; 
+import React, {useEffect, useState} from 'react'; 
 import { Provider } from "react-redux";
+import { Spinner } from 'native-base';
+import * as Font from 'expo-font';
 import Routes from './routes'; 
 import  Store  from "./redux/store";
+import {ROBOTO_FONT, ROBOTO_MEDIUM_FONT} from './const'
 const store=Store();
-export default () =>(
+const ROBOTO= require('../node_modules/native-base/Fonts/Roboto.ttf');
+const ROBOTO_MEDIUM= require('../node_modules/native-base/Fonts/Roboto_medium.ttf');
+export default () =>{
+const [fontsLoaded, setFontsLoaded] = useState(false);
+
+useEffect(()=>{
+  if (!fontsLoaded){
+    loadFonts();
+  }
+},[fontsLoaded]);  
+const loadFonts = async () => {
+  await Font.loadAsync({
+    [ROBOTO_FONT]: ROBOTO,
+    [ROBOTO_MEDIUM_FONT] : ROBOTO_MEDIUM
+  });
+  setFontsLoaded(true);
+}
+if (!fontsLoaded){
+  return <Spinner/>
+}
+return(
   <Provider store={store}>
    <Routes/>   
   </Provider>  
-)
+);}
