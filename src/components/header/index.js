bf9cafe3b8f8faefd1c485e7a3d3a0563adff624
ckubotaQ <ckubota@quaxar.com>
2020-12-18 09:11:34 -0600

    ajustando doc

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,4 +1,8 @@
 import React from "react";
+/**
+ * 
+ * @type {Routes}
+ */
 import { Header, Left, Body, Right, Thumbnail } from "native-base";
  export default function MyHeader ({ImageUri}) {
      return(
