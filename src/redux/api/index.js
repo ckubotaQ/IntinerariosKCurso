bf2af95187abc7ebebf10468775f18c828c3c48a
ckubotaQ <ckubota@quaxar.com>
2020-12-23 08:33:09 -0600

    add moment.js

--- src/redux/api/index.js
+++ src/redux/api/index.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 const baseURL = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices";
+/**
+ * APi
+ * https://rapidapi.com/skyscanner/api/skyscanner-flight-search?endpoint=5a9ca8d2e4b084deb4ea61a9
+ * @required {country } {currency} {locale} {originplace} {destinationplace} {outboundpartialdate}
+ * @option {inboundpartialdate}
+ */
 export default (url, method, data, headers) => 
     axios ({
         baseURL,
